6cd0e3ca8be3ba14ea0d25c8f81027c858ae0dd4
rayli1639 <ray.z.li@gmail.com>
2021-10-23 12:20:13 -0400

    More backend

--- backend/api.py
+++ backend/api.py
@@ -2,29 +2,75 @@ from flask import Flask
 from flask import request
 import json
 import datetime
-from examplePost import examplePost
-from exampleGet import exampleGet
+from ncrPost import ncrPost
+from ncrGet import ncrGet
 
 app = Flask(__name__)
 
 nepOrganization = "test-drive-e605ad46ec584ec5b0f25"
 
+serviceURL = "gateway-staging.ncrcloud.com"
+
 @app.route('/login', methods = ['POST'])
 def login():
-    data = request.get_json() # Has username and password
-    payload = {"searchCriteria": {"profileUsername": data.username, 
-                "socialSecurityNumber": data.password}, "operator": "AND", 
-                "pageStart": 0, "pageSize": 10}
-    headers = {
-        "accept": "application/json",
-        "content-type": "application/json",
-        "nep-organization": nepOrganization
-    }
+    if request.method == 'POST':
+        data = request.get_json() # Has username and password
+        payload = {"searchCriteria": {"profileUsername": data.username, 
+                    "socialSecurityNumber": data.password}, "operator": "AND", 
+                    "pageStart": 0, "pageSize": 10}
+        res = ncrPost(data=payload, requestURL="https://gateway-staging.ncrcloud.com/cdm/consumers/find")
+        if res['status'] == 200:
+            if len(res['consumers']) == 1: # One consumer per username and password
+                consumer = res['consumers'][0]
+                CAN = consumer['consumerAccountNumber']
+                consumerData = {'firstName': consumer['firstName'],
+                                'lastName': consumer['lastName']
+                }
+                # Log them in
+        else:
+            return "Error contacting NCR customer API"
 
+@app.route('/register', methods = ['POST'])
+def register():
+    if request.method == 'POST':
+        data = request.get_json()
+        payload = {
+            'profileUsername': data.username,
+            'firstName': data.firstName,
+            'lastName': data.lastName,
+            'socialSecurityNumber': data.password
+        }
+        res = ncrPost(data=payload, requestURL=serviceURL + "/cdm/consumers")
+        if res['status'] == 200:
+            # Log them in
+            return
+        else:
+            return 'There was an error registering with those credentials'
+
+@app.route('/createCatalogItem', methods = ['POST'])
+def createCatalogItem():
+    if request.method == 'POST':
+        data = request.get_data()
+        payload = {
+            'version': 0,
+            'shortDescription': {
+                'values': {
+                    'locale': 'en-US',
+                    'value': data.NFTData
+                }
+            }
+        }
+        uniqueID = "HACKCDRYNFTID"
+        res = ncrPost(data=payload, requestURL=serviceURL + "/catalog/v2/items/" + uniqueID)
+        if res['status'] == 200:
+            return "Successful"
+        else:
+            return "Failed to upload catalog item"
+            
 data = {'username':'username', 'password': 'password'} # Has username and password
 payload = {"searchCriteria": {"profileUsername": data['username'], 
             "socialSecurityNumber": data['password']}, "operator": "AND", 
             "pageStart": 0, "pageSize": 10}
-print(examplePost(data=payload))
-
+res = ncrPost(data=payload, requestURL=serviceURL + "/cdm/consumers/find")
+print(res['status'])
     
--- backend/ncrPost.py
+++ backend/ncrPost.py
@@ -4,16 +4,15 @@ from datetime import datetime
 from datetime import timezone
 from hmacHelper import hmacHelper
 
-def examplePost(secretKey="8a92f9468d494a60bb7c830017a3be65", 
-                sharedKey="dd162e750ae14a48960e00c4b55222f4", 
+def examplePost(secretKey="0a0bf40d942b4fe4a5ed82417a3799cf", 
+                sharedKey="9f33fd5e7e3f40608efc96c47275e94a", 
                 nepOrganization="test-drive-e605ad46ec584ec5b0f25",
-                data = {}):
+                data = {},
+                requestURL="https://api.ncr.com/security/authentication/login"):
     
     now = datetime.now(tz=timezone.utc)
     now = datetime(now.year, now.month, now.day, hour=now.hour,
                    minute=now.minute, second=now.second)
-
-    requestURL = "https://api.ncr.com/security/authentication/login"
     httpMethod = 'POST'
     contentType = 'application/json'
 
